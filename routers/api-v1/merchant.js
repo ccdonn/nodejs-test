0bb260809aaad0202801ef76ea1f4bc4a556310a
chingche.lin <chingche.lin@RTWPC10108.intra.rakuten.co.jp>
2016-10-19 11:54:05 +0800

    update

--- routers/api-v1/merchant.js
+++ routers/api-v1/merchant.js
@@ -1,22 +1,9 @@
-var cors = require('cors');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var urlencode = require('urlencode');
 var multipart = require('connect-multiparty');
 var multipartMiddleware = multipart();
 
 
 module.exports = function(app, router) {
-  // var webRoutes = express.Router();
   app.use('/api/v1/merchant', router);
-  router.use(cookieParser());
-  router.use(bodyParser.urlencoded({extended:true}));
-  router.use(bodyParser.json());
-
-  router.use(cors({
-    methods: [ 'GET','HEAD','PUT','PATCH','POST','DELETE','OPTIONS' ]
-  }));
-  // router.use(cors());
 
   router.route('')
     .get(function(req, res){
@@ -46,4 +33,24 @@ module.exports = function(app, router) {
       res.send({status:'_OK'});
     });
     // .options(cors(), function(req, res){});
+
+  router.route('/resort')
+    .post(function(req, res){
+      console.info('POST /api/v1/merchant/resort');
+      res.send({status:'_OK'});
+    });
+
+  router.route('/category')
+    .get(function(req, res){
+      console.info('GET /api/v1/merchant/category');
+      res.send({status:'_OK'});
+    });
+
+  router.route('/:id')
+    .get(function(req, res){
+      console.info('id='+req.params.id);
+      console.info('GET /api/v1/merchant/:id');
+      res.send({status:'_OK'});
+    });
+
 }
--- server-sep.js
+++ server-sep.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var app = express();
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var urlencode = require('urlencode');
+
+var db = require('./config');
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cors({
+  methods: [ 'GET','HEAD','PUT','PATCH','POST','DELETE','OPTIONS' ]
+}));
 
 require('./routers/api-v1/merchant')(app, express.Router());
 require('./routers/api-v1/coupon')(app, express.Router());
-
-var db = require('./config');
 
 app.listen(3333);
 
